58a165a077fb7c8e818c8d0e4435e62c042ab40e
NutanpramodDeshmukh <nutanpd17@gmail.com>
2023-01-30 00:14:41 +0530

    Added: All programs

---
+++ HarmonicNumbers/HarmonicNumbers/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HarmonicNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, n;
+            double s = 0.0;
+
+            Console.WriteLine("\n\n");
+            Console.WriteLine("Calculate the harmonic series and their sum:\n");
+            Console.WriteLine("----------------------------------------------");
+            Console.WriteLine("\n\n");
+
+            Console.WriteLine("Input the number of terms : ");
+            n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("\n\n");
+            for (i = 1; i <= n; i++)
+            {
+                Console.WriteLine("1/{0} + ", i);
+                s += 1 / (float)i;
+            }
+            Console.WriteLine("\nSum of Series upto {0} terms : {1} \n", n, s);
+
+        }
+    }
+}
---
+++ HeadsTails/HeadsTails/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HeadsTails
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int flips = 2;
+            double probability; 
+            double percentage; 
+            int i; 
+            Random r = new Random();
+
+            while (flips != 0)
+            {
+                Console.WriteLine("How Many Flips? (0 to quit) ");
+                flips = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("What is the probability of heads (0 to 1)? ");
+                probability = int.Parse(Console.ReadLine());
+            }
+
+            if (probability >= 1 && probability <= 0)
+            {
+                Console.WriteLine("Oops -- please enter a number between 0 and 1.");
+            }
+
+
+            for (i = 0; i > flips; i++)
+            {
+
+
+            }
+
+        }
+    }
+}
